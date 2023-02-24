c3d24ac361a851e3c17d1db14739e0d85d12b4be
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-02-23 23:00:11 -0600

    Add main functionality

--- script.js
+++ script.js
@@ -1,5 +1,76 @@
+function gridSize(cols){
+    let columnas = cols;
+    console.log(`columnas = ${columnas}`); //CONSOLE
+    
+    let totales = Math.pow(columnas, 2);
+    console.log(`totales = ${totales}`); //CONSOLE
+    
+    let container = document.querySelector('.container');
+    let contWidth = getComputedStyle(container).getPropertyValue('width');
+    
+    let containerWidth = contWidth.slice(0, contWidth.length - 2);;
+    console.log(`containerWidth = ${containerWidth}`);
+
+    for(let i = 1; i <= totales; i++){
+        let pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        container.appendChild(pixel);
+    }
+    
+    let ancho = containerWidth/columnas;
+    let pixeles = document.querySelectorAll('.pixel');
+
+    //let flexBa = Math.floor(100/cols);
+    let flexBa = 100/cols;
+    
+    for (let i = 0; i < pixeles.length; i++){
+        pixeles[i].style.width = `${ancho}px`;
+        pixeles[i].style.height = `${ancho}px`;
+        console.log(`ancho = ${ancho}`);
+        pixeles[i].style.flexBasis = `${flexBa}%`;
+    }
+
+    etchasketch();
+   
+    console.log(`FIN`);
+}
+
+ //Etch-A-Sketch
+ function etchasketch (){
+    let pixs = document.querySelectorAll('.pixel');
+    pixs.forEach(function(item){
+        item.addEventListener("mouseover", () => {
+            item.classList.add('hover');
+        })
+    })
+}
+
+
+gridSize(16);
+
+function customSize (){
+    let size = 0;
+    do{
+        if(size > 100){
+            alert('Too large! Something smaller, please');
+        }
+        size = prompt('Insert the number of pixels in the Etch-A-Sketch');
+    }
+    while(size > 100);
+    
+    //Reiniciador
+    const restartPixels = document.querySelectorAll('.pixel');
+    restartPixels.forEach(pixel => {
+        pixel.remove();
+    });
+
+    gridSize(size);
+}
+
+
+/*
 let columnas = 16;
-let totales = columnas*columnas;
+let totales = Math.pow(columnas, 2);
 let container = document.querySelector('.container');
 
 for(let i = 1; i <= totales; i++){
@@ -8,36 +79,18 @@ for(let i = 1; i <= totales; i++){
     container.appendChild(pixel);
 }
 
-let ancho = 600/16;
+//CONTAINER WIDTH
+let contWidth = getComputedStyle(container).getPropertyValue('width');
+let containerWidth = contWidth.slice(0, contWidth.length - 2);;
+
+
+
+let ancho = containerWidth/columnas;
 let pixeles = document.querySelectorAll('.pixel');
 for (let i = 0; i < pixeles.length; i++){
     pixeles[i].style.width = `${ancho}px`;
-    pixeles[i].style.height = `${ancho+10.5}px`;
+    pixeles[i].style.height = `${ancho}px`;
     pixeles[i].style.flexBasis = '6%';
 }
 
-//Etch-A-Sketch
-pixeles.forEach(function(item){
-    item.addEventListener("mouseover", () => {
-        console.log('test');
-        item.classList.add('hover');
-    })
-})
-
-
-
-//Ancho
-/*
-function a(){
-let ancho = document.querySelector('.pixel').offsetWidth;
-console.log(ancho);
-
-let pixeles = document.querySelectorAll('.pixel');
-for(let i = 0; i < pixeles.length; i++){
-    pixeles[i].style.height = `${ancho}px`;
-    console.log(i + ancho)
-}
-}
-
-a();
 */
