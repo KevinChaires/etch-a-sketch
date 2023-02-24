cd2d855acfd7effc05dfd5dde2f6a0d9f53d1e73
Kevin Chaires <37939130+KevinChaires@users.noreply.github.com>
2023-02-24 01:39:16 -0600

    Add pretty UI:

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ function gridSize(cols){
     for(let i = 1; i <= totales; i++){
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
+        pixel.setAttribute("id", i);
         container.appendChild(pixel);
     }
     
     let ancho = containerWidth/columnas;
     let pixeles = document.querySelectorAll('.pixel');
 
-    //let flexBa = Math.floor(100/cols);
     let flexBa = 100/cols;
     
     for (let i = 0; i < pixeles.length; i++){
@@ -31,12 +31,12 @@ function gridSize(cols){
     }
 
     etchasketch();
-   
-    console.log(`FIN`);
-}
-
- //Etch-A-Sketch
+    
+ 
+ 
+    //Etch-A-Sketch
  function etchasketch (){
+
     let pixs = document.querySelectorAll('.pixel');
     pixs.forEach(function(item){
         item.addEventListener("mouseover", () => {
@@ -45,18 +45,34 @@ function gridSize(cols){
     })
 }
 
+   
+    console.log(`FIN`);
+}
+
+
+
+
+
+
+
+
+
+
 
 gridSize(16);
 
 function customSize (){
-    let size = 0;
+    let size = 16;
     do{
         if(size > 100){
             alert('Too large! Something smaller, please');
         }
+        else if (size <= 0){
+            alert('Too small! Something larger, please');
+        }
         size = prompt('Insert the number of pixels in the Etch-A-Sketch');
     }
-    while(size > 100);
+    while(size > 100 || size <= 0);
     
     //Reiniciador
     const restartPixels = document.querySelectorAll('.pixel');
@@ -67,30 +83,19 @@ function customSize (){
     gridSize(size);
 }
 
+/* Random Colors
+function etchasketchrgb (){
 
-/*
-let columnas = 16;
-let totales = Math.pow(columnas, 2);
-let container = document.querySelector('.container');
-
-for(let i = 1; i <= totales; i++){
-    let pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    container.appendChild(pixel);
+    
+    let pixs = document.querySelectorAll('.pixel');
+    pixs.forEach(function(item){
+        item.addEventListener("mouseover", () => {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            item.style.backgroundColor = `rgb(${r},${g},${b})`;
+            //item.classList.add('hover');
+        })
+    })
 }
-
-//CONTAINER WIDTH
-let contWidth = getComputedStyle(container).getPropertyValue('width');
-let containerWidth = contWidth.slice(0, contWidth.length - 2);;
-
-
-
-let ancho = containerWidth/columnas;
-let pixeles = document.querySelectorAll('.pixel');
-for (let i = 0; i < pixeles.length; i++){
-    pixeles[i].style.width = `${ancho}px`;
-    pixeles[i].style.height = `${ancho}px`;
-    pixeles[i].style.flexBasis = '6%';
-}
-
 */
